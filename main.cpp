a0c152798bccf7e5ddd1e450ac9aa6257342c34a
Moontop <snail0525@naver.com>
2018-09-20 15:18:44 -0700

    Added constants for random number limits and changed type for variables in readReading()

--- main.cpp
+++ main.cpp
@@ -3,14 +3,17 @@
 #include <random>
 #include <iomanip>
 using namespace std;
-
+const int DOUBLEMAX = 90;
+const int DOUBLEMIN = 50;
+const int INTMAX = 1024;
+const int INTMIN = 512;
 
 void createReading(){
     ofstream outputFile("Reading.txt");
     random_device rd;
     mt19937 generator(rd());
-    uniform_int_distribution<>intDistribution(512, 1024);
-    uniform_real_distribution<double>doubleDistribution(50, 90);
+    uniform_int_distribution<>intDistribution(INTMIN, INTMAX);
+    uniform_real_distribution<double>doubleDistribution(DOUBLEMIN, DOUBLEMAX);
 
     int limit = intDistribution(generator);
     for(int i = 0; i < limit; i++) {
@@ -22,19 +25,19 @@ void createReading(){
 void readReading(){
     ifstream inputFile("Reading.txt");
     int numberOfReading = 0;
-    int averge;
-    int highest;
-    int lowest;
-    int median;
+    double averge = 0;
+    double highest = 0;
+    double lowest = 0;
+    double median = 0;
 
     double num;
     while (inputFile >> num){
         if(numberOfReading++ % 2 == 1) {
             cout << num << endl;
+
         }
     }
     numberOfReading /= 2;
-    numberOfReading--;
 
     cout << "There are "<< numberOfReading << " readings in the file." << endl;
 }
