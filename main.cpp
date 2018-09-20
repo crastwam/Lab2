86dcc7ff92efa5459d525b0093480055e0580d3f
Moontop <snail0525@naver.com>
2018-09-20 12:58:00 -0700

    Creates a reading file and puts random int

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <random>
 using namespace std;
 
+
+void createReading(){
+    ofstream outputFile("Reading.txt");
+    random_device rd;
+    mt19937 generator(rd());
+    uniform_int_distribution<>distribution(0, 9);
+
+    for(int i =0; i < 5; i++) {
+        outputFile << distribution(generator) << endl;
+    }
+}
+
+
 int main() {
-    cout << "Andrew" << endl;
+    createReading();
     return 0;
 }
