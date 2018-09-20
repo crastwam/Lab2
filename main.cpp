160f9fbc265d112b510cc297be462c1bf85de379
Moontop <snail0525@naver.com>
2018-09-20 13:12:13 -0700

    Creates a proper reading file that matches lab description

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <random>
+#include <iomanip>
 using namespace std;
 
 
@@ -8,10 +9,12 @@ void createReading(){
     ofstream outputFile("Reading.txt");
     random_device rd;
     mt19937 generator(rd());
-    uniform_int_distribution<>distribution(0, 9);
+    uniform_int_distribution<>intDistribution(512, 1024);
+    uniform_real_distribution<double>doubleDistribution(50, 90);
 
-    for(int i =0; i < 5; i++) {
-        outputFile << distribution(generator) << endl;
+    for(int i = 0; i < intDistribution(generator); i++) {
+        outputFile << i << " ";
+        outputFile << fixed << setprecision(3) << doubleDistribution(generator) << endl;
     }
 }
 
